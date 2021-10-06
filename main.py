84d06c30a30287a1a9f095aa461106b75f0cedf4
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-06 14:15:39 +0500

    Add Face Mesh

--- main.py
+++ main.py
@@ -7,8 +7,19 @@ cap = cv2.VideoCapture(0)
 current_time = 0
 previous_time = 0
 
+face_mesh_model = mp.solutions.face_mesh
+face_mesh = face_mesh_model.FaceMesh()
+draw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = face_mesh.process(imgRGB)
+
+    if results.multi_face_landmarks:
+        for face_landmark in results.multi_face_landmarks:
+            draw.draw_landmarks(img, face_landmark)
 
     current_time = time.time()
     fps = 1/(current_time - previous_time)
