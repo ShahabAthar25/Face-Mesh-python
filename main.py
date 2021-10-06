89baab2e4b177a89c2641f4d6701e2db72b3b120
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-06 14:06:24 +0500

    Add FPS

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+current_time = 0
+previous_time = 0
+
 while True:
     success, img = cap.read()
+
+    current_time = time.time()
+    fps = 1/(current_time - previous_time)
+    previous_time = current_time
+
+    cv2.putText(img, f"FPS: {int(fps)}", (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), )
 
     cv2.imshow("Face Mesh", img)
 
